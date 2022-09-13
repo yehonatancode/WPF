31b2608f9255199e8df87076ba2ac76f1cc32c84
Yehonatan <yehonatantseva@gmail.com>
2022-09-13 12:33:47 +0300

    progress in API

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,12 +14,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Azure.Core;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public class CoinsAPI {
+        static async Task Main(string[] args)
+        {
+            using(System.Net.Http.HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://api.exchangeratesapi.io/v1/")
+                    response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    
+}
     public partial class MainWindow : Window
     {
         public MainWindow()
